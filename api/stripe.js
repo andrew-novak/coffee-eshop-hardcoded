df93b6037a0e4f9b3fa967585e17e2058f8dfbb7
andrew-novak <contact@andrewnovak.co.uk>
2023-08-29 16:56:27 +0100

    fixed: parentheses added to STRIPE_SECRET import in api/stripe.js

--- api/stripe.js
+++ api/stripe.js
@@ -1,6 +1,6 @@
 const Stripe = require("stripe");
 
-const STRIPE_SECRET = require("./constants/env.js");
+const { STRIPE_SECRET } = require("./constants/env.js");
 
 const stripe = Stripe(STRIPE_SECRET);
 
